e492f5d99d8181241d26231724113df10475aa2c
madandan <mdd12138@163.com>
2020-07-06 11:30:05 +0800

    单例模式

---
+++ src/DesignPattern/SingletonPattern/SingleObject.java
@@ -0,0 +1,15 @@
+package DesignPattern.SingletonPattern;
+
+public class SingleObject {
+    private static SingleObject instance = new SingleObject();
+
+    private SingleObject(){}
+
+    public static SingleObject getInstance(){
+        return instance;
+    }
+
+    public void showMessage(){
+        System.out.println("Hello Worle!");
+    }
+}
---
+++ src/DesignPattern/SingletonPattern/SingletonPatternDemo.java
@@ -0,0 +1,8 @@
+package DesignPattern.SingletonPattern;
+
+public class SingletonPatternDemo {
+    public static void main(String[] args) {
+        SingleObject object = SingleObject.getInstance();
+        object.showMessage();
+    }
+}
