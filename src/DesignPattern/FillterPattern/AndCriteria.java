275330d585deae8b5c0189311da613f00390da2e
madandan <mdd12138@163.com>
2020-07-10 15:05:30 +0800

    过滤器模式

---
+++ src/DesignPattern/FillterPattern/AndCriteria.java
@@ -0,0 +1,19 @@
+package DesignPattern.FillterPattern;
+
+import java.util.List;
+
+public class AndCriteria implements Criteria{
+    private final Criteria criteria;
+    private final Criteria otherCriteria;
+
+    public AndCriteria(Criteria criteria, Criteria otherCriteria){
+        this.criteria = criteria;
+        this.otherCriteria = otherCriteria;
+    }
+
+    @Override
+    public List<Person> meetCriteria(List<Person> persons) {
+        List<Person> firstCriteriaPersons = criteria.meetCriteria(persons);
+        return otherCriteria.meetCriteria(firstCriteriaPersons);
+    }
+}
